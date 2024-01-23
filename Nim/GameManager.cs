b5393843ad11df128aab7308fa47dfaba95ed687
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2024-01-23 12:41:03 -0700

    Assets and Resolution
    I got all the audio and sprite assets into the game and set the resolution to the window so that it's not tiny.

--- Nim/GameManager.cs
+++ Nim/GameManager.cs
@@ -9,6 +9,8 @@ namespace Nim
 		private GraphicsDeviceManager _graphics;
 		private SpriteBatch _spriteBatch;
 
+		private Texture2D Cookie;
+
 		bool isPlaying = false;
 		int turn = 0;
 
@@ -21,7 +23,13 @@ namespace Nim
 			_graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
 			IsMouseVisible = true;
-		}
+
+            if (GraphicsDevice == null) _graphics.ApplyChanges();
+
+            _graphics.PreferredBackBufferWidth = GraphicsDevice.Adapter.CurrentDisplayMode.Width;
+            _graphics.PreferredBackBufferHeight = GraphicsDevice.Adapter.CurrentDisplayMode.Height;
+            _graphics.ApplyChanges();
+        }
 
 		protected override void Initialize()
 		{
@@ -35,6 +43,8 @@ namespace Nim
 			_spriteBatch = new SpriteBatch(GraphicsDevice);
 
 			// TODO: use this.Content to load your game content here
+
+			Cookie = Content.Load<Texture2D>("Sprites/OatmealCookie");
 		}
 
 		protected override void Update(GameTime gameTime)
@@ -52,6 +62,12 @@ namespace Nim
 			GraphicsDevice.Clear(Color.CornflowerBlue);
 
 			// TODO: Add your drawing code here
+
+			_spriteBatch.Begin();
+
+			_spriteBatch.Draw(Cookie, new Vector2(400, 240), Color.White);
+
+			_spriteBatch.End();
 
 			base.Draw(gameTime);
 		}
